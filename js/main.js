0b8a0e4a2f5735d7347a98a8cdd3027117d948df
poly28 <horimasa0328@gmail.com>
2021-04-16 23:44:51 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// ID:open,closeの読み込み(ハンバーガーメニュー操作)
+const $open = document.getElementById('open');
+const $overlay = document.querySelector('.overlay');
+const $close = document.getElementById('close');
+
+$open.addEventListener('click', () => {
+	$overlay.classList.add('show');
+	$open.classList.add('hide');
+});
+$close.addEventListener('click', () => {
+	$overlay.classList.remove('show');
+	$open.classList.remove('hide');
+});
