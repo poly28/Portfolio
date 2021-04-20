58b7c30526efc81eefcb21ea801892718af17a4b
poly28 <horimasa0328@gmail.com>
2021-04-20 14:56:29 +0900

    workのデザイン変更

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-'use strict'
+'use strict';
 
 // ID:open,closeの読み込み(ハンバーガーメニュー操作)
 const $open = document.getElementById('open');
 const $overlay = document.querySelector('.overlay');
 const $close = document.getElementById('close');
+const $mask = document.querySelector('.mask');
+const $work1 = document.getElementById('work1');
+
+let isMask = false;
 
 $open.addEventListener('click', () => {
 	$overlay.classList.add('show');
@@ -12,4 +16,16 @@ $open.addEventListener('click', () => {
 $close.addEventListener('click', () => {
 	$overlay.classList.remove('show');
 	$open.classList.remove('hide');
+});
+
+$work1.addEventListener('click', () => {
+	$mask.classList.remove('hide');
+	$mask.classList.add('show');
+	isMask = true;
+	if (isMask === true) {
+		$mask.addEventListener('click', () => {
+			$mask.classList.remove('show');
+			$mask.classList.add('hide');
+		});
+	}
 });
